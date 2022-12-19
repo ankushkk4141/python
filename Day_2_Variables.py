27fffbbeada486ee9e943d44585a7425f79bf645
Ankush Singh <66295776+ankushkk4141@users.noreply.github.com>
2022-12-19 10:34:21 +0530

    Create Day_2_Variables.py

---
+++ Day_2_Variables.py
@@ -0,0 +1,23 @@
+# Variables
+
+first_name = 'Ankush'
+last_name = 'singh'
+Age = 26
+skills = ['Cloud', 'K8S', 'Helm']
+person_info = {
+  'first_name' = 'Ankush',
+  'last_name' = 'singh',
+  'Age' = 26
+}
+
+# Printing values stored in variables
+
+print('First Name: ', first_name)
+print('Last Name: ', last_name)
+print('Age: ', Age)
+print('Person Info: ', person_info)
+
+# Declaring multiple variable in one line
+
+first, second, Age, Male = 'Ankush', 'Singh', 26, True
+print(first, second, Age, Male)
